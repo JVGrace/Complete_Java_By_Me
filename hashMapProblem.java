2e84a4f5ac64247ff62d1e8fb0b6a27a93d3255a
JVGrace <pradumn.patel3101@gmail.come>
2024-11-06 22:49:06 +0530

    Hash Map And a simple problem

---
+++ hashMapProblem.java
@@ -0,0 +1,23 @@
+/* Using Hashmap and printing custom output by using iterator and Map interface
+ * 
+ */
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class hashMapProblem {
+    public static void main(String[] args) {
+        HashMap hm=new HashMap<>();
+        hm.put(101, "news");
+        hm.put(109, "args");
+        hm.put(132, "Wow");
+        Set s= hm.entrySet();
+        Iterator itr=s.iterator();
+        while (itr.hasNext()) {
+           Map.Entry m=(Map.Entry)itr.next();
+            System.out.println();
+        }
+    }
+}
